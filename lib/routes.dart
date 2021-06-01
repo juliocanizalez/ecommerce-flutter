c506750c4710742788b66636c2df8377c93d3534
Julio Canizalez <juliocanizalez@outlook.com>
2021-05-31 18:02:50 -0600

    [UPDATE] routes for navigation

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_kiara/screens/complete_profile/complete_profile_screen.dart';
+import 'package:ecommerce_kiara/screens/otp/otp_screen.dart';
+import 'package:ecommerce_kiara/screens/sign_up/sign_up_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ecommerce_kiara/screens/splash/splash_screen.dart';
 import 'package:ecommerce_kiara/screens/sign_in/sign_in_screen.dart';
@@ -5,4 +8,7 @@ import 'package:ecommerce_kiara/screens/sign_in/sign_in_screen.dart';
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
 };
